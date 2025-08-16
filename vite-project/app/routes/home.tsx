837c03181f3bc224ec7c0b26ee65f486d8141481
Shifa200 <shifa20naqvi@gmal.com>
2025-08-16 23:23:44 +0530

    made progress-navbar resumecard

--- vite-project/app/routes/home.tsx
+++ vite-project/app/routes/home.tsx
@@ -1,13 +1,26 @@
+import Navbar from "~/components/navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your Dream Job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        {!loadingResumes && resumes?.length === 0 ? (
+            <h2>No resumes found. Upload your first resume to get feedback.</h2>
+        ): (
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        )}
+      </div>
+    </section>
+  </main>;
 }
